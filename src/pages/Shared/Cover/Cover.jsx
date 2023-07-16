0fce62581c9af72ea75aa8826b3a6138b9533dad
shahin2525 <shahin451999@gmail.com>
2023-07-15 21:13:48 -0700

    readme

--- src/pages/Shared/Cover/Cover.jsx
+++ src/pages/Shared/Cover/Cover.jsx
@@ -12,8 +12,8 @@ const Cover = ({ img, title }) => {
           <h1 className="mb-5 text-5xl font-bold">{title}</h1>
           <p className="mb-5">
             Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            excepturi exercitationem quasi. areff hamtan aut repudiandae et a id
+            nisi.
           </p>
         </div>
       </div>
